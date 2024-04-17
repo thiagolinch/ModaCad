8c2e898b220ed4fb1fb02e50c3d3283cb02034b2
Thiago Linchin <79920937+devthiagolinch@users.noreply.github.com>
2024-04-17 19:49:19 -0400

    Atualizar o tagRoutes.routes.ts (adicionando rota de post)

--- back-end/src/Shared/http/Routes/tagRoutes.routes.ts
+++ back-end/src/Shared/http/Routes/tagRoutes.routes.ts
@@ -6,4 +6,8 @@ tagRoutes.get("/", (request, response) => {
     return console.log("Tag router")
 })
 
+tagRoutes.post("/", (request, return) => (
+    return console.log("tag post route")
+))
+
 export  { tagRoutes }
