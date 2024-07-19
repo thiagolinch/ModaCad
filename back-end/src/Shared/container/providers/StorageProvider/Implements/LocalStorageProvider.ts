c27bdef9fc900cd6f531cb25cb3a546efb34a20d
Thiago Linchin <dev.thiagolinch@gmail.com>
2024-07-19 13:39:35 -0400

    upload confi#02

---
+++ back-end/src/Shared/container/providers/StorageProvider/Implements/LocalStorageProvider.ts
@@ -0,0 +1,31 @@
+import { IStorageProvider } from "../IStorageProvider";
+import fs from "fs";
+import { resolve } from "path";
+
+import upload from "../../../../../Config/upload/upload";
+
+
+
+class LocalStorageProvider implements IStorageProvider {
+
+    async save(file: string, folder: string): Promise<string> {
+        await fs.promises.rename(
+            resolve(upload.tmpFolder, file),
+            resolve(`${upload.tmpFolder}/${folder}`, file)
+        );
+
+        return file;
+    }
+    async delete(file: string, folder: string): Promise<void> {
+        const filename = resolve(`${upload.tmpFolder}/${folder}`, file);
+
+        try {
+            await fs.promises.stat(filename);
+        } catch {
+            return;
+        }
+    }
+
+}
+
+export { LocalStorageProvider }
