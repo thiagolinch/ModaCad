999ac7379a27ade3a237025ac77646a10f048b23
Thiago Linchin <dev.thiagolinch@gmail.com>
2024-07-24 14:14:14 -0400

    cors#02

--- back-end/src/Shared/http/app.ts
+++ back-end/src/Shared/http/app.ts
@@ -1,27 +1,18 @@
 import 'reflect-metadata';
 import cors from "cors"
-
-import "../TypeOrm/data-source"
-
-import createConnection from "../TypeOrm/data-source"
-import "../container"
-
 import express from "express";
 
+import "../TypeOrm/data-source"
+import createConnection from "../TypeOrm/data-source"
+import "../container"
 import { routes } from './Routes';
 
 createConnection();
 const app = express();
 
-const corsOption = {
-    origin: ['http://localhost:3000'],
-    credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE"],
-}
-app.use(cors(corsOption));
-
 
 app.use(express.json());
+app.use(cors())
 
 app.use(routes)
 
