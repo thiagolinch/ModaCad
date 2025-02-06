226504cc8fe726ba61787ee9993f52762dc3bd7d
Thiago Linchin <dev.thiagolinch@gmail.com>
2025-02-06 13:13:25 -0400

    REFACTOR: remove interface IRequest não utilizada no WebhookUseCase

--- src/Modules/Admins/useCases/Pagamentos/webhook/webHookUseCase.ts
+++ src/Modules/Admins/useCases/Pagamentos/webhook/webHookUseCase.ts
@@ -3,23 +3,6 @@ import { IMercadoPagoProvider } from "../../../../../Shared/container/providers/
 import { IAdminsRepository } from "../../../repositories/IAdminsRepository";
 import { IPlansRepository } from "../../../../Posts/repository/IPlansRepository";
 
-interface IRequest {
-    live_mode: boolean;
-    type: string;
-    action: string;
-    payment_id: string;
-    api_version: string;
-    data: string;
-    user_id: string;
-    date_created: string;
-    event_id: string;
-    date: string;
-    application_id: string;
-    entity: string;
-    version: string;
-    id: string;
-}
-
 @injectable()
 export class WebhookUseCase {
     constructor(
